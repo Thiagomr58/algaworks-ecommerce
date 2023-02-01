39583f05984eb9fcaa84dddbf1dd348a66a5df2a
ThiagoRodrigues <thiagomr58@outlook.com>
2023-01-31 21:55:41 -0300

    Ajustando caminho do import

--- src/test/java/com.algaworks.ecommerce.iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com.algaworks.ecommerce.iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
